07dd9709eae8078f3bec71af4360aa9dd355c830
Avatar <codecj93@gmail.com>
2024-08-02 10:31:23 -0400

    useMutation hook inputted into searchbooks.jsx

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -11,6 +11,8 @@ import {
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -60,7 +62,13 @@ const SearchBooks = () => {
   };
 
   // create function to handle saving a book to our database
+  const [saveBook] = useMutation(SAVE_BOOK);
+
+
   const handleSaveBook = async (bookId) => {
+    
+
+    
     // find the book in `searchedBooks` state by the matching id
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
@@ -72,7 +80,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      await saveBook({ variables: { input: bookToSave } });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
